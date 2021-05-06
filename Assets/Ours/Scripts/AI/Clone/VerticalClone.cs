c59719b68ec32ac94a231fe3fa655d68db916e77
Wkamfar <wkamfar@gmail.com>
2021-05-06 17:59:29 -0400

    I added the ability to kill the vertical enemy, and gave a dysfucntional gun.

--- Assets/Ours/Scripts/AI/Clone/VerticalClone.cs
+++ Assets/Ours/Scripts/AI/Clone/VerticalClone.cs
@@ -6,6 +6,7 @@ public class VerticalClone : MonoBehaviour
 {
     public GameObject player;
     public float speed = 4f;
+    public int verticalEnemyLives = 5;
 
 
     // Start is called before the first frame update
@@ -19,5 +20,26 @@ public class VerticalClone : MonoBehaviour
     {
         Vector3 Targetpos = new Vector3(this.transform.position.x, player.transform.position.y, 5);
         transform.position = Vector2.MoveTowards(transform.position, Targetpos, speed * Time.deltaTime);
+        //Checking Health
+        if (verticalEnemyLives <= 0)
+        {
+            this.gameObject.SetActive(false);
+            Destroy(this);
+        }
+    }
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if (col.gameObject.tag == "SwordBullet")
+        {
+            verticalEnemyLives--;
+            Debug.Log(verticalEnemyLives);
+        }
+    }
+    void OnTriggerStay2D (Collider2D col)
+    {
+        if (col.CompareTag("AimZone"))
+        {
+            Debug.Log("Aiming!");
+        }
     }
 }
