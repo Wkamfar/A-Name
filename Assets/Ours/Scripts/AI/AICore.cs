290e1d49c710da07a6bea3135d1d970349699c97
Wkamfar <wkamfar@gmail.com>
2021-05-26 16:04:15 -0400

    We added the spawner to the game, and stored some more data in the AICore.

--- Assets/Ours/Scripts/AI/AICore.cs
+++ Assets/Ours/Scripts/AI/AICore.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AICore 
 {
+    //Booleans
+    private bool exist;
     //Integer Variables
     private static int numOfAI = 0;
     private int hitPoints;
@@ -16,6 +18,8 @@ public class AICore
         lifespan = ls;
         hitPoints = hp;
         lifeElapsed = 0f;
+        numOfAI++;
+        exist = true;
     }
 
     public void elapseTime(float timeChange)
@@ -31,6 +35,17 @@ public class AICore
             hitPoints = 0;
         }
     }
-
+    public int getNumberOfAI()
+    {
+        return numOfAI;
+    }
+    public void died()
+    {
+        if (!this.isAlive() && exist)
+        {
+            numOfAI--;
+            exist = false;
+        }
+    }
     public bool isAlive() { if (hitPoints == 0 || lifeElapsed >= lifespan) return false; return true; }
 }
